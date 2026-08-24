c12dd57915127ab5836e277fac4af30890ec52fc
alamryalamry367 <alamryalamry367@gmail.com>
2026-08-24 06:55:46 +0300

    Use static robots.txt for Google crawling

--- server.js
+++ server.js
@@ -399,16 +399,6 @@ app.get('/tiktok-video-downloader', (_, res) =>
   res.sendFile(path.join(PUBLIC, 'tiktok-video-downloader.html'))
 );
 
-app.get('/robots.txt', (_, res) => {
-  res.type('text/plain');
-  res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.send(
-    'User-agent: *\n' +
-    'Allow: /\n' +
-    'Sitemap: https://clipflow-node-1.onrender.com/sitemap.xml\n'
-  );
-});
-
 app.get('/sitemap.xml', (_, res) =>
   res.type('application/xml').send(
     `<?xml version="1.0" encoding="UTF-8"?>
